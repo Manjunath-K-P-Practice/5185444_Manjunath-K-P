2f462edcd781d09db2cf0f79677c084dfe74a576
Manjunath-K-P-Practice <manjunathkp7411@gmail.com>
2025-08-21 12:22:23 +0530

    modified c file

--- Hackerrank/week3/merge_two_sorted_linked_lists.c
+++ Hackerrank/week3/merge_two_sorted_linked_lists.c
@@ -98,9 +98,7 @@ SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNo
         head=head2;
         head2=head2->next;
     }
-    tail=head;
-
-    
+    tail=head;    
     while(head1!=NULL && head2!=NULL) 
     {
         if(head1->data<=head2->data) {
@@ -113,7 +111,6 @@ SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNo
         }
         tail =tail->next;
     }
-
     
     if(head1 !=NULL)
      {

ffaf13c091d68d62bec2f25804c1a9e665ae50f2
Manjunath-K-P-Practice <manjunathkp7411@gmail.com>
2025-08-22 10:38:21 +0530

    added testing output of lonely integer problem to git

--- Hackerrank/week1/Mini-Max_Sum.c
+++ Hackerrank/week1/Mini-Max_Sum.c
@@ -29,11 +29,10 @@ void miniMaxSum(int arr_count, int* arr) {
       int i, j, temp;
 
     for (i = 0; i < n - 1; i++) {
-        // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++) {
-            // Swap if current element is greater than the next
+            
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
+        
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
